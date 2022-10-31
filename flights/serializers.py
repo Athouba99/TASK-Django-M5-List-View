a84082bff0684b47790005377c38b93242bb3850
athouba99 <athooba99@gmail.com>
2022-10-31 22:25:40 +0300

    details from Task2

--- flights/serializers.py
+++ flights/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Booking, Flight
 
 # in DRF place the models in this file 
+
+# Task 1 
 class FlightListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flight 
@@ -11,3 +13,9 @@ class BookingListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = ["flight","date","id"]
+
+# Task 2 
+class BookingDetalListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        feilds = ["id","flight","date","passengers"]
--- flights/views.py
+++ flights/views.py
@@ -2,7 +2,7 @@
 from pytz import timezone
 from rest_framework.generics import ListAPIView
 from .models import Booking, Flight
-from flights.serializers import FlightListSerializer,BookingListSerializer 
+from flights.serializers import FlightListSerializer,BookingListSerializer, BookingDetalListSerializer 
 from django.utils import timezone
 
 class FlightListView(ListAPIView): #1rt view
@@ -17,3 +17,9 @@ class BookingListView(ListAPIView): #3rd view
     queryset = Booking.objects.filter(date__gt=timezone.now()) # filter the fetching only for upcoming boking by date, not for all the fields
     serializer_class = BookingListSerializer # user the same serializer as 2nd view, using the same field to display 
 
+# Task 2
+class BookingDetailListView(ListAPIView):#4th view
+    queryset = Booking.objects.all()
+    serializer_class = BookingDetalListSerializer 
+    lookup_field = "id", #identifying the objects
+    lookup_url_kwarg = "object_id"
