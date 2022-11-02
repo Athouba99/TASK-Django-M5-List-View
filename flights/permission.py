f4434d85676443c6da86cbc8d228279b9db2806a
athouba99 <athooba99@gmail.com>
2022-11-02 22:00:26 +0300

    solving task 5, No.6 is not done yet

--- flights/permission.py
+++ flights/permission.py
@@ -1,6 +1,6 @@
 from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView
 from flights.models  import Booking, Flight  
-from .serializers import FlightListSerializer 
+from .serializers import FlightListSerializer,CreateFlightSerilazer 
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 
 class ListView(ListAPIView):
@@ -9,4 +9,13 @@ class ListView(ListAPIView):
     permission_classes = [AllowAny]  #any one can access
 
 class CreateView(CreateAPIView):
-    serializer_class = 
+    serializer_class = CreateFlightSerilazer
+    permission_classes = [IsAuthenticated]
+
+class DeleteView(DestroyAPIView):
+    queryset = Booking
+    serializer_class = FlightListSerializer
+    lookup_field = "id"
+    lookup_url_kwarg = "object_id"
+    permission_classes = [IsAuthenticated,IsAdminUser]
+  
--- users/permossion.py
+++
@@ -1,3 +0,0 @@
-from rest_framework.generics import 
-from flights import Flight, Booking 
-from .serializer import 
